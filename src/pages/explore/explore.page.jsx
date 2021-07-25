cb222ad96cce9a740e0acd8982cdec0df49b089c
VQN <quangvinh.nguyen1218@gmail.com>
2021-07-26 03:25:15 +0700

    [explore-1574]: implement horizontal scroll

--- src/pages/explore/explore.page.jsx
+++ src/pages/explore/explore.page.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext, useCallback } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import FormInput from 'components/form-input/form-input.component';
@@ -12,44 +12,169 @@ import useDebounce from 'shared/hooks/useDebounce';
 import SVG from 'react-inlinesvg';
 import SharedBreadcrumb from 'components/shared-breadcrumb/shared-breadcrumb.component';
 import { toAbsoluteUrl } from '../../shared/utils/assetsHelper';
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
+import { categoryIconMapper } from './icon.mapper';
 
-const Symbol48px = styled.div`
-  width: 48px;
-  height: 48px;
-`;
+const CategoryIcon = function ({ ...props }) {
+  const { imgSrc, fill } = props;
 
-const CategoryItem = function ({ itemName, bgColor, iconSrc, url, isFirst }) {
   return (
-    <>
-      <Link
-        to={url}
-        className={`d-flex flex-column align-items-center ${!isFirst && 'ms-4'}`}
+    <div
+      className='rounded-circle d-flex justify-content-center align-items-center'
+      style={{ width: '48px', height: '48px', backgroundColor: '#F3F4F4' }}
+    >
+      <div className='d-block'>
+        <SVG src={toAbsoluteUrl(imgSrc)} style={{ width: '24px', height: '24px', marginTop: '-2px', fill: `${fill}` }}></SVG>
+      </div>
+    </div>
+  );
+};
+
+const CategoryItem = function ({ ...props }) {
+  const { imgSrc, value, isFirst } = props;
+  return (
+    <div className={''.concat(!isFirst && 'ms-3')} tabIndex={0}>
+      <div className='d-flex justify-content-center align-items-center'>
+        <CategoryIcon imgSrc={imgSrc}></CategoryIcon>
+      </div>
+      <div
+        className='text-center mt-1'
+        style={{ width: '72px', height: 'auto', wordWrap: 'break-word', fontSize: '12px', lineHeight: '.8rem' }}
+      >
+        <span className=''>{value}</span>
+      </div>
+    </div>
+  );
+};
+
+const ArrowRight = function ({ ...props }) {
+  const { isLastItemVisible, scrollNext, visibleItemsWithoutSeparators } = useContext(VisibilityContext);
+  const [disabled, setDisabled] = useState(!visibleItemsWithoutSeparators.length && isLastItemVisible);
+  useEffect(() => {
+    if (visibleItemsWithoutSeparators.length) {
+      setDisabled(isLastItemVisible);
+    }
+  }, [isLastItemVisible, visibleItemsWithoutSeparators]);
+
+  return (
+    <button
+      type='button'
+      onClick={function (evt) {
+        scrollNext();
+      }}
+      disabled={disabled}
+      style={{ border: '0', backgroundColor: `rgba(0,0,0,0)` }}
+    >
+      <div
+        className='rounded-circle d-flex justify-content-center align-items-center bg-light'
         style={{
-          width: '110px',
-          lineHeight: '1rem',
+          width: '32px',
+          height: '32px',
+          filter: `drop-shadow(0px 4px 8px rgba(13, 27, 30, 0.1))`,
+          cursor: 'pointer',
         }}
       >
-        <Symbol48px
-          className='d-flex justify-content-center align-items-center rounded-circle mb-2'
-          style={{
-            backgroundColor: `${bgColor}`,
-          }}
-        >
-          <SVG src={toAbsoluteUrl(`${iconSrc}`)}></SVG>
-        </Symbol48px>
-        <div className='text-center'>
-          <span>{itemName}</span>
+        <div className='d-block'>
+          <SVG src={toAbsoluteUrl('/assets/commons/vector-right.svg')} style={{ width: '24px', height: '24px', marginTop: '-2px' }}></SVG>
         </div>
-      </Link>
-    </>
+      </div>
+    </button>
   );
+};
+
+const ArrowLeft = function ({ ...props }) {
+  const { isFirstItemVisible, scrollPrev, visibleItemsWithoutSeparators } = useContext(VisibilityContext);
+  const [disabled, setDisabled] = useState(!visibleItemsWithoutSeparators.length && isFirstItemVisible);
+  useEffect(() => {
+    if (visibleItemsWithoutSeparators.length) {
+      setDisabled(isFirstItemVisible);
+    }
+  }, [isFirstItemVisible, visibleItemsWithoutSeparators]);
+
+  return (
+    <button
+      type='button'
+      onClick={function (evt) {
+        scrollPrev();
+      }}
+      disabled={disabled}
+      style={{ border: '0', backgroundColor: `rgba(0,0,0,0)` }}
+    >
+      <div
+        className='rounded-circle d-flex justify-content-center align-items-center bg-light'
+        style={{
+          width: '32px',
+          height: '32px',
+          filter: `drop-shadow(0px 4px 8px rgba(13, 27, 30, 0.1))`,
+          transform: `rotate(180deg)`,
+          cursor: 'pointer',
+        }}
+      >
+        <div className='d-block'>
+          <SVG src={toAbsoluteUrl('/assets/commons/vector-right.svg')} style={{ width: '24px', height: '24px', marginTop: '-2px' }}></SVG>
+        </div>
+      </div>
+    </button>
+  );
+};
+
+const ScrollMenuV1 = function ({ ...props }) {
+  // return (
+  //   <ScrollMenu
+  //   // arrowLeft={ArrowLeft}
+  //   // arrowRight={ArrowRight}
+  //   // data={function () {
+  //   //   Object.keys(categoryIconMapper).map(function (currCategory, index) {
+  //   //     return (
+  //   //       <CategoryItem
+  //   //         key={categoryIconMapper[currCategory].id}
+  //   //         imgSrc={categoryIconMapper[currCategory].iconSrc}
+  //   //         value={categoryIconMapper[currCategory].name}
+  //   //         isFirst={index === 0}
+  //   //       ></CategoryItem>
+  //   //     );
+  //   //   });
+  //   // }}
+  //   // dragging={true}
+  //   // hideArrows={hideArrows}
+  //   // hideSingleArrow={hideSingleArrow}
+  //   // onFirstItemVisible={this.onFirstItemVisible}
+  //   // onLastItemVisible={this.onLastItemVisible}
+  //   // onSelect={this.onSelect}
+  //   // onUpdate={this.onUpdate}
+  //   // ref={(el) => (this.menu = el)}
+  //   // scrollToSelected={scrollToSelected}
+  //   // selected={selected}
+  //   // transition={+0.3}
+  //   // translate={0}
+  //   // wheel={true}
+  //   />
+  // );
+};
+
+const onWheel = function (apiObj, evt) {
+  const isThouchpad = Math.abs(evt.deltaX) !== 0 || Math.abs(evt.deltaY) < 15;
+
+  if (isThouchpad) {
+    evt.stopPropagation();
+    return;
+  }
+
+  if (evt.deltaY < 0) {
+    apiObj.scrollNext();
+  } else if (evt.deltaY > 0) {
+    apiObj.scrollPrev();
+  }
 };
 
 const ExplorePage = () => {
   const [shopCategories, setShopCategories] = useState([]);
+  const [position, setPosition] = React.useState(0);
+
   const [getAllShopCategories, { loading: getAllShopCategoriesLoading, error: getAllShopCategoriesError, data: getAllShopCategoriesData }] =
     useLazyQuery(GET_ALL_SHOP_CATEGORIES);
-  // console.log("test", !shopCategories?.length)
+  console.log('test', shopCategories);
+
   useEffect(() => {
     if (!shopCategories?.length) {
       getAllShopCategories();
@@ -58,6 +183,19 @@ const ExplorePage = () => {
       }
     }
   }, [getAllShopCategories, getAllShopCategoriesData, shopCategories]);
+
+  const restorePosition = useCallback(
+    function ({ scrollContainer }) {
+      if (scrollContainer.current) {
+        scrollContainer.current.scrollLeft = position;
+      }
+    },
+    [position]
+  );
+
+  const savePosition = React.useCallback(function ({ scrollContainer }) {
+    !!scrollContainer.current && setPosition(scrollContainer.current.scrollLeft);
+  }, []);
 
   return (
     <>
@@ -95,10 +233,34 @@ const ExplorePage = () => {
             <h1 className='fw-bold'>Explore Shops</h1>
           </div>
         </div>
-        <div aria-label='category-selection-bar' className='row pt-4 pb-4'>
-          <div className='d-flex justify-content-start align-items-start'>
-            <CategoryItem itemName='Food & Beverages' bgColor='#FFA800' iconSrc='/assets/food.svg' url='/' isFirst></CategoryItem>
-          </div>
+        <div aria-label='category-selection-bar' className='row pt-4 pb-4' style={{ width: '300px' }}>
+          {/* <div className='d-flex bg-danger' style={{ overflow: 'hidden', width: '300px'  }}>
+            <CategoryItem imgSrc='/assets/categories/food.svg' value='test'></CategoryItem>
+            {Object.keys(categoryIconMapper).map(function (currCategory, index) {
+              return (
+                <CategoryItem
+                  key={categoryIconMapper[currCategory].id}
+                  imgSrc={categoryIconMapper[currCategory].iconSrc}
+                  value={categoryIconMapper[currCategory].name}
+                  isFirst={index === 0}
+                ></CategoryItem>
+              );
+            })}
+            <button type='button' style={{ position: 'absolute' }} onClick={{}}>next</button>;
+          </div> */}
+
+          <ScrollMenu LeftArrow={ArrowLeft} RightArrow={ArrowRight}>
+            {Object.keys(categoryIconMapper).map(function (currCategory, index) {
+              return (
+                <CategoryItem
+                  key={categoryIconMapper[currCategory].id}
+                  imgSrc={categoryIconMapper[currCategory].iconSrc}
+                  value={categoryIconMapper[currCategory].name}
+                  isFirst={index === 0}
+                ></CategoryItem>
+              );
+            })}
+          </ScrollMenu>
         </div>
 
         <div aria-label='title' className='row pt-2'>
@@ -234,7 +396,6 @@ const ExplorePage = () => {
               </div>
             </div>
           </div>
-          
         </div>
       </div>
     </>
--- src/pages/explore/icon.mapper.jsx
+++ src/pages/explore/icon.mapper.jsx
@@ -1,92 +1,91 @@
-const categoryIconMapper = {
+export const categoryIconMapper = {
   201: {
-
     id: '201',
     name: 'Điện tử',
     englishName: 'Electronics',
     description: '',
+    iconSrc: '/assets/categories/monitor.svg',
   },
   202: {
-
     id: '202',
     name: 'Thời trang & phụ kiện',
     englishName: 'Fashion & Accessories',
     description: '',
+    iconSrc: '/assets/categories/t-shirt.svg',
   },
   203: {
-
     id: '203',
     name: 'Nhà cửa & đời sống',
     englishName: 'Home, Kitchen & Garden',
     description: '',
+    iconSrc: '/assets/categories/vacuum-cleaner.svg',
   },
   204: {
-
     id: '204',
     name: 'Ô tô, xe máy & linh kiện',
     englishName: 'Automotive & Accessories',
     description: '',
+    iconSrc: '/assets/categories/car.svg',
   },
   205: {
-
     id: '205',
     name: 'Sách, Điện ảnh & Âm nhạc (Giải trí)',
     englishName: 'Books, Movies & Music (Entertainment)',
     description: '',
+    iconSrc: '/assets/categories/media.svg',
   },
   206: {
-
     id: '206',
     name: 'Thú cưng',
     englishName: 'Pet',
     description: '',
+    iconSrc: '/assets/categories/pet.svg',
   },
   207: {
-
     id: '207',
     name: 'Sưu tầm & nghệ thuật',
     englishName: 'Collectibles & Art',
     description: '',
+    iconSrc: '/assets/categories/stamp.svg',
   },
   208: {
-
     id: '208',
     name: 'Đồ chơi & Games',
     englishName: 'Toys & Games',
     description: '',
   },
   209: {
-
     id: '209',
     name: 'Thương mại & Công nghệ',
     englishName: 'Business & Industrial',
     description: '',
   },
   210: {
-
     id: '210',
     name: 'Sức khỏe & Sắc đẹp',
     englishName: 'Health & Beauty',
     description: '',
+    iconSrc: '/assets/categories/bandage.svg',
   },
   211: {
-
     id: '211',
     name: 'Thể thao',
     englishName: 'Sporting Goods',
     description: '',
+    iconSrc: '/assets/categories/muscle.svg',
   },
   212: {
-    
     id: '212',
     name: 'Du lịch',
     englishName: 'Travel',
     description: '',
+    iconSrc: '/assets/categories/luggage.svg',
   },
   299: {
     id: '299',
     name: 'Khác',
     englishName: 'Other',
     description: '',
+    iconSrc: '/assets/categories/box.svg',
   },
 };
