005044bb45a8b92b66c814ca7a6d828a9895abf7
VQN <quangvinh.nguyen1218@gmail.com>
2021-08-01 02:13:43 +0700

    refactor code, implement new footer

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,4 +1,4 @@
-import FooterSection from 'components/footer/footer.component';
+import Footer from 'components/footer/footer-v2.component';
 import Header from 'components/header/header-v2.component';
 import React from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
@@ -7,10 +7,10 @@ const MainLayout = function ({ children }) {
   const location = useLocation();
   const history = useHistory();
   return (
-    <div>
+    <div className='d-flex flex-column' style={{ minHeight: '100vh' }}>
       <Header history={history} />
       <div className='body-container'>{children}</div>
-      <FooterSection />
+      <Footer />
     </div>
   );
 };
