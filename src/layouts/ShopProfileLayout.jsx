c622d5c24f208497b4ff3a10d07ab7dcf8816ca0
VQN <quangvinh.nguyen1218@gmail.com>
2021-08-02 20:31:16 +0700

    test og

--- src/layouts/ShopProfileLayout.jsx
+++ src/layouts/ShopProfileLayout.jsx
@@ -9,11 +9,31 @@ const ShopProfileLayout = function ({ children }) {
   const history = useHistory();
   return (
     <div className='d-flex flex-column' style={{ backgroundColor: '#F3F4F4', minHeight: '100vh' }}>
-      {/* <Helmet>
-        <meta name='og:description' content={`test description`} />
-        <meta property='og:title' content={`title`} />
-        <meta property='og:image' content={`https://vcdn-vnexpress.vnecdn.net/2021/07/07/Aventador-Ultimae-Coupe-1-3961-1625659942.jpg`} />
-      </Helmet> */}
+      <Helmet>
+        <meta property='og:url' content='http://dev.soby.vn/shop-profile/8b3991f8-2fb6-430d-91ac-3aca51a7f54c' />
+        <meta property='og:title' content='Mototo - Rating: 9.4' />
+        <meta
+          property='og:image'
+          content='https://cdn-dev.soby.vn/shops/8b3991f8-2fb6-430d-91ac-3aca51a7f54c/df84106f-6905-42ca-baff-0ae22742cb5e.jpeg'
+        />
+        <meta property='og:image:width' content='475' />
+        <meta property='og:image:height' content='308' />
+        <meta property='og:image:type' content='image/jpg' />
+        <meta
+          property='og:description'
+          content="243 reviews for Mototo, rating 9.4: 'Our customers love unique statement pieces. Please add more. I loved the colored pearls and matching bracelets and acetate earrings today. Thanks for great selection!'"
+        />
+        <meta property='og:type' content='website' />
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta
+          property='twitter:description'
+          content="243 reviews for Mototo, rating 9.4: 'Our customers love unique statement pieces. Please add more. I loved the colored pearls and matching bracelets and acetate earrings today. Thanks for great selection!'"
+        />
+        <meta
+          property='twitter:image'
+          content='https://cdn-dev.soby.vn/shops/8b3991f8-2fb6-430d-91ac-3aca51a7f54c/df84106f-6905-42ca-baff-0ae22742cb5e.jpeg'
+        />
+      </Helmet>
       <Header history={history} />
       <div className='' style={{ backgroundColor: '#F3F4F4', width: '100vw' }}>
         {children}
