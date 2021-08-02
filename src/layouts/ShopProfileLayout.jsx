47e7013fa0822afb91829ba0ec0d1b17c603e0ac
VQN <quangvinh.nguyen1218@gmail.com>
2021-08-02 18:45:20 +0700

    test

--- src/layouts/ShopProfileLayout.jsx
+++ src/layouts/ShopProfileLayout.jsx
@@ -9,11 +9,11 @@ const ShopProfileLayout = function ({ children }) {
   const history = useHistory();
   return (
     <div className='d-flex flex-column' style={{ backgroundColor: '#F3F4F4', minHeight: '100vh' }}>
-      <Helmet>
+      {/* <Helmet>
         <meta name='og:description' content={`test description`} />
         <meta property='og:title' content={`title`} />
         <meta property='og:image' content={`https://vcdn-vnexpress.vnecdn.net/2021/07/07/Aventador-Ultimae-Coupe-1-3961-1625659942.jpg`} />
-      </Helmet>
+      </Helmet> */}
       <Header history={history} />
       <div className='' style={{ backgroundColor: '#F3F4F4', width: '100vw' }}>
         {children}
